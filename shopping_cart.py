a1d692992c1cfce2c85e370534bec733763d4a58
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-06-04 00:42:43 -0300

    add failing test for cart itens validation

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,17 +1,18 @@
 from typing import List
 
+
 class ShoppingCart:
-  def __init__(self) -> None:
-    self.items: List[str] = []
+    def __init__(self) -> None:
+        self.items: List[str] = []
 
-  def add(self, item: str):
-    self.items.append(item)
+    def add(self, item: str):
+        self.items.append(item)
 
-  def size(self) -> int:
-    return len(self.items)
-  
-  def get_items(self) -> List[str]:
-    pass
+    def size(self) -> int:
+        return len(self.items)
 
-  def get_total_price(self, price_map):
-    pass
+    def get_items(self) -> List[str]:
+        pass
+
+    def get_total_price(self, price_map):
+        pass
--- test_shopping_cart.py
+++ test_shopping_cart.py
@@ -2,6 +2,12 @@ from shopping_cart import ShoppingCart
 
 
 def test_can_add_item_to_cart():
-  cart = ShoppingCart()
-  cart.add("apple")
-  assert cart.size() == 1
+    cart = ShoppingCart()
+    cart.add("apple")
+    assert cart.size() == 1
+
+
+def test_when_item_added_then_cart_contains_item():
+    cart = ShoppingCart()
+    cart.add("apple")
+    assert "apple" in cart.get_items()
