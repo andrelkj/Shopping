83b3e7d870cf362e52470ddfab7bca70319dbd9b
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-06-04 01:11:50 -0300

    fix bug

--- test_shopping_cart.py
+++ test_shopping_cart.py
@@ -16,7 +16,7 @@ def test_when_item_added_then_cart_contains_item():
 
 def test_when_add_more_than_max_items_should_fail():
     cart = ShoppingCart(5)
-    for _ in range(6):
+    for _ in range(5):
         cart.add("apple")
 
     with pytest.raises(OverflowError):
