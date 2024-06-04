5b216d560fe06577b5ee8bdebdffb412efe2fb5c
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-06-04 00:25:31 -0300

    add shopping cart

---
+++ shopping_cart.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class ShoppingCart:
+  def __init__(self) -> None:
+    pass
+
+  def add(self, item: str):
+    pass
+
+  def size(self) -> int:
+    return 0
+  
+  def get_items(self) -> List[str]:
+    pass
+
+  def get_total_price(self, price_map):
+    pass
